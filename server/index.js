20e9c16d33e22fa6051fa9ba57fb424b3823d85b
yashwithatutorials <yashwithareddy1212@gmail.com>
2025-06-05 11:47:37 +0530

    added database for login and signup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const EmployeeModel=require('./models/Employee')
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/employee");
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body;
+    EmployeeModel.findOne({email : email})
+    .then(user=>{
+        if(user){
+            if(user.password===password){
+                res.json("success");
+            }
+            else{
+                res.json("password incorrect");
+            }
+        }
+        else{
+            res.json("not exist");
+        }
+    })
+})
+app.post('/signup',(req,res)=>{
+    EmployeeModel.create(req.body)
+    .then(employees=>res.json(employees))
+    .catch(err=>res.json(err))
+})
+app.listen(8080,()=>{
+    console.log("server is running");
+})
